4ffb30ed075cf54122967916ca987d4172b7ece6
danielacapassi <danielacapassi_nutricao@hotmail.com>
2020-10-27 00:48:56 -0300

    criação da página fazer denuncia

--- scripts/registro.js
+++ scripts/registro.js
@@ -1,21 +1,30 @@
-function enviar() {
+function login() {
     if (termos.checked === false) {
         alert("Você deve aceitar os termos de uso para se registrar.")
     } else if (
-        nome.value.length==0 ||
-        cpf.value.length==0 ||
-        telefone.value.length==0 ||
-        email.value.length==0 ||
-        nasc.value.length==0 ||
-        estado.value.length==0 ||
-        cidade.value.length==0||
-        senha.value.length==0
-        ) {
-            alert("Você deve inserir todos os dados")
-    } else if(senha.value != confsenha.value){
-       alert("A senha e a confirmação são diferentes")
+        nome.value.length == 0 ||
+        cpf.value.length == 0 ||
+        telefone.value.length == 0 ||
+        email.value.length == 0 ||
+        nasc.value.length == 0 ||
+        estado.value.length == 0 ||
+        cidade.value.length == 0 ||
+        senha.value.length == 0
+    ) {
+        alert("Você deve inserir todos os dados")
+    } else if (senha.value != confsenha.value) {
+        alert("A senha e a confirmação são diferentes")
     } else {
         //ENVIO DE DADOS BACKEND
     }
 
 }
+
+function login() {
+    if (email.value.length == 0 || senha.value.length == 0 || ) {
+        alert("Você deve inserir todos os dados.")
+    } else if (senha == false) {
+        alert("Senha inválida!")
+    } else {
+        alert("Login realizado com sucesso!")
+    }
